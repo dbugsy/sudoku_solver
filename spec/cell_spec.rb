af61f844f9bc721c0decc0906531dc0989a8ba1e
DLDB <dpbergquist@gmail.com>
2014-04-27 16:01:12 +0100

    created outline of expected tests

---
+++ spec/cell_spec.rb
@@ -0,0 +1,15 @@
+require 'cell'
+
+describe Cell do
+  
+  xit 'can have a value' do
+  end
+
+  xit 'can have a list of possible values' do
+  end
+
+  xit 'knows its neighbours (1 box, 1 row, 1 column)' do
+  end
+
+
+end
---
+++ spec/grid_spec.rb
@@ -0,0 +1,20 @@
+require 'grid'
+
+describe Grid do
+  
+  let(:grid){Grid.new}
+
+  it 'contains an array with 81 elements' do
+    expect(grid.cells.count).to eq 81
+  end
+
+  xit 'creates boxes, columns and rows' do
+  end
+
+  xit 'reads cell contents' do
+  end
+
+  xit 'solves the puzzle' do
+  end
+
+end
